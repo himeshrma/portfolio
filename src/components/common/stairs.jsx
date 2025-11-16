b5437c17e2141a80ec15a124fc543c5ac6436e60
himeshrma <sharmahimesh2003@gmail.com>
2025-11-17 01:11:27 +0530

    About page

---
+++ src/components/common/stairs.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Stairs = () => {
+  const stairParentRef = useRef(null);
+  useGSAP(() => {
+    const tl = gsap.timeline();
+
+    tl.to(stairParentRef.current, {
+      display: "block",
+    });
+
+    tl.from(".stair", {
+      height: 0,
+      stagger: { amount: -0.25 },
+    });
+
+    tl.to(".stair", {
+      y: "100%",
+      stagger: { amount: -0.25 },
+    });
+
+    tl.to(stairParentRef.current, {
+      display: "none",
+    });
+
+    tl.to(".stair", {
+      y: "0%",
+    });
+  });
+  return (
+    <div ref={stairParentRef} className="h-screen w-full  fixed z-20 top-0">
+      <div className="h-full w-full flex ">
+        <div className=" stair h-full w-1/5 bg-black"></div>
+        <div className=" stair h-full w-1/5 bg-black "></div>
+        <div className=" stair h-full w-1/5 bg-black"></div>
+        <div className=" stair h-full w-1/5 bg-black "></div>
+        <div className=" stair h-full w-1/5 bg-black"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Stairs;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import Stairs from "./components/common/stairs.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <Stairs />
       <App />
     </BrowserRouter>
   </StrictMode>
